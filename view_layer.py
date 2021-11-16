70bb6921f3cadd05c8c5bb7d6c60c0543b4079b2
Gorgious <nathan.hild@gmail.com>
2021-11-16 17:04:03 +0100

    Simplify view ops

--- view_layer.py
+++ view_layer.py
@@ -1,60 +1,40 @@
 import bpy
 
 
-class GU_OT_view_layer_load_stored_view(bpy.types.Operator):
-    bl_label = "Load View Layer Stored View"
-    bl_idname = "view_layer.load_stored_view"
-    index: bpy.props.IntProperty()
+class GU_OT_view_layer_save_or_load_stored_view(bpy.types.Operator):
+    bl_label = "Save or Load View Layer Stored View"
+    bl_idname = "view_layer.save_or_load_stored_view"
+    index: bpy.props.IntProperty(description="Stored View Index")
+    save_or_load: bpy.props.BoolProperty(description= "False : Save, True : Load")
 
     @classmethod
     def poll(cls, context):
+        # Check if at least one View 3D area exists in the current screen
         if any(a for a in context.screen.areas if a.type == "VIEW_3D"):
             return True
         else:
-            cls.poll_message_set("No 3D Viewport available")
+            if bpy.app.version >= (3, 0, 0):
+                cls.poll_message_set("No 3D Viewport available")
             return False
 
-
     def execute(self, context):
-        from space_view3d_stored_views.core import POV
-
-        view_3d_area = next(a for a in context.screen.areas if a.type == "VIEW_3D")
-        view = context.scene.stored_views.view_list[self.index]
-
-        pov = POV(self.index)
-        pov.view3d = view_3d_area.spaces[0] # Patching this since it references a wrong space data
-        pov.update_v3d(view.pov)
-
-        return {"FINISHED"}
-
-
-class GU_OT_view_layer_save_stored_view(bpy.types.Operator):
-    bl_label = "Save View Layer Stored View"
-    bl_idname = "view_layer.save_stored_view"
-    index: bpy.props.IntProperty()
-
-    @classmethod
-    def poll(cls, context):
-        if any(a for a in context.screen.areas if a.type == "VIEW_3D"):
-            return True
-        else:
-            cls.poll_message_set("No 3D Viewport available")
-            return False
-
-
-    def execute(self, context):
+        # Import the POV class from stored views add-on which offers view saving & loading features
         from space_view3d_stored_views.core import POV
 
         view_3d_area = next(a for a in context.screen.areas if a.type == "VIEW_3D")
         if self.index >= 0:
             view = context.scene.stored_views.view_list[self.index]
         else:
+            # We add a new entry since this view layer is not registered
             view = context.scene.stored_views.view_list.add()
             view.name = context.view_layer.name
 
         pov = POV(self.index)
-        pov.view3d = view_3d_area.spaces[0] # Patching this since it references a wrong space data
-        pov.from_v3d(view.pov)
+        pov.view3d = view_3d_area.spaces[0]  # Patching this since it references a wrong space data
+        if self.save_or_load:
+            pov.update_v3d(view.pov)
+        else:
+            pov.from_v3d(view.pov)
 
         return {"FINISHED"}
 
@@ -71,6 +51,7 @@ class GU_PT_view_layer_stored_view(bpy.types.Panel):
         layout = self.layout
 
         if not hasattr(scene, "stored_views"):
+            # Check if add-on is registered
             if hasattr(bpy.types, "VIEW3D_OT_stored_views_initialize"):
                 layout.operator("view3d.stored_views_initialize")
             else:
@@ -79,13 +60,25 @@ class GU_PT_view_layer_stored_view(bpy.types.Panel):
             return
         
         view_layer = context.view_layer
+        # If the view layer's view has already been saved, we find its entry index
         index = -1
         for i, view in enumerate(scene.stored_views.view_list):
             if view_layer.name == view.name:
                 index = i
                 break
+
         row = layout.row()
-        row.operator("view_layer.save_stored_view", icon="EXPORT", text="Save").index = index
+        save_op = row.operator("view_layer.save_or_load_stored_view", icon="EXPORT", text="Save")
+        save_op.index = index
+        save_op.save_or_load = False
+
         sub_row = row.row()
-        sub_row.operator("view_layer.load_stored_view", icon="IMPORT", text="Load").index = index
-        sub_row.enabled = index >= 0
+        load_op = sub_row.operator("view_layer.save_or_load_stored_view", icon="IMPORT", text="Load")
+        load_op.index = index
+        load_op.save_or_load = True
+        sub_row.enabled = index >= 0  # Prevent loading view if it doesn't exist
+
+
+if __name__ == "__main__":
+    bpy.utils.register_class(GU_OT_view_layer_save_or_load_stored_view)
+    bpy.utils.register_class(GU_PT_view_layer_stored_view)
