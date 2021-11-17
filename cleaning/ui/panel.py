f705789f5b195b5fae58493344295aee8a761748
Gorgious <nathan.hild@gmail.com>
2021-11-17 12:39:58 +0100

    Add subpanel for driver cleaning

--- cleaning/ui/panel.py
+++ cleaning/ui/panel.py
@@ -11,7 +11,21 @@ class GU_PT_clean_scene(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("clean.faulty_drivers")
-        layout.operator("clean.update_drivers")
+
         layout.operator("clean.remove_fake_users")
         layout.operator("clean.remove_single_empties")
+
+
+class GU_PT_clean_scene_drivers(bpy.types.Panel):
+    bl_label = "Drivers"
+    bl_idname = "GU_PT_clean_scene_drivers"
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
+    bl_context = "scene"
+    bl_parent_id = "GU_PT_clean_scene"
+    bl_options = {'DEFAULT_CLOSED'}
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("clean.faulty_drivers", text="Clean")
+        layout.operator("clean.update_drivers", text="Update")
