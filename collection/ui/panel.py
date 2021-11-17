7666a6c7b20b4d5cd2a1cd4f3adb6bbd288bb20c
Gorgious <nathan.hild@gmail.com>
2021-11-17 16:46:43 +0100

    Add exclude/include globally operator in collection properties panel

--- collection/ui/panel.py
+++ collection/ui/panel.py
@@ -1,16 +1,27 @@
 import bpy
-from gorgious_utilities.collection.operator import (
-    duplicate_hierarchy_only,
+from gorgious_utilities.collection.helper import (
+    get_collection_layer_from_collection,
 )
+
 
 class GU_PT_collection_properties_utilities(bpy.types.Panel):
     bl_label = "Utilities"
     bl_idname = "GU_PT_collection_properties_utilities"
-    bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
+    bl_space_type = "PROPERTIES"
+    bl_region_type = "WINDOW"
     bl_context = "collection"
 
     def draw(self, context):
         layout = self.layout
 
-        layout.operator(duplicate_hierarchy_only.GU_OT_collection_duplicate_hierarchy_only.bl_idname)
+        layout.operator("collection.duplicate_hierarchy_only")
+
+        col = context.collection
+        layer_col = get_collection_layer_from_collection(context.view_layer, col)
+        exclude = layer_col.exclude
+        op = layout.operator(
+            "collection.include_or_exclude_from_all_view_layers",
+            text="Exclude Globally" if not exclude else "Include Globally",
+        )
+        op.col_name = col.name
+        op.exclude = not exclude
