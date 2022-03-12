6387165bf7cffd5f72508881b703e6d8f74b0382
Gorgious <nathan.hild@gmail.com>
2022-03-12 18:04:18 +0100

    Add option to set default value to factor input

--- nodes/operators/create_factor_input.py
+++ nodes/operators/create_factor_input.py
@@ -6,12 +6,17 @@ class GU_OT_nodes_Hidden(bpy.types.Operator, NodeTreeOperator):
     bl_idname = "nodes.create_factor_input"
     bl_label = "Create Factor Input"
 
+    default_value: bpy.props.FloatProperty(name="Default Value", min=0, max=1)
+
     @classmethod
     def poll(cls, context):
         active_node_tree = get_active_node_tree(context)
         if active_node_tree is None:
             return False
         return isinstance(active_node_tree, (bpy.types.GeometryNodeTree, bpy.types.ShaderNodeTree))
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
 
     def execute(self, context):
         tree = get_active_node_tree(context)
@@ -27,7 +32,7 @@ class GU_OT_nodes_Hidden(bpy.types.Operator, NodeTreeOperator):
             print("Node tree not supported for factor input")
             return {"FINISHED"}
         new_mix = nodes.new(mix_type)
-        new_mix.inputs[0].default_value = 0
+        new_mix.inputs[0].default_value = self.default_value
         tree.links.new(input.outputs[-1], new_mix.inputs[0])
         nodes.remove(new_mix)
         return {"FINISHED"}
