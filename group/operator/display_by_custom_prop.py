ec0556f398ab7b06a9ce72346500c3ba9e772955
Gorgious <nathan.hild@gmail.com>
2022-04-19 10:46:34 +0200

    Ensure custom group colors are far away enough from each other

--- group/operator/display_by_custom_prop.py
+++ group/operator/display_by_custom_prop.py
@@ -1,7 +1,8 @@
-from random import uniform, random, seed
-import bpy
-from bpy.types import Operator, Collection
-from bpy.props import EnumProperty, StringProperty, PointerProperty
+from collections import defaultdict
+import numpy as np
+from mathutils import Color
+from bpy.types import Operator
+from bpy.props import EnumProperty, StringProperty
 
 
 class GU_OT_display_by_custom_prop(Operator):
@@ -18,21 +19,15 @@ class GU_OT_display_by_custom_prop(Operator):
         return context.window_manager.invoke_props_dialog(self)
 
     def execute(self, context):
+        groups = defaultdict(list)
         for obj in context.selected_objects:
-            if self.prop_name in obj:
-                seed(hash(obj[self.prop_name]))
-                switch = random()
-                if switch < 0.333:
-                    r = uniform(0, 0.8)
-                    g = uniform(r, 1)
-                    b = 1 - g - r
-                elif switch < 0.666:
-                    g = uniform(0, 0.8)
-                    b = uniform(g, 1)
-                    r = 1 - g - b
-                else:
-                    b = uniform(0, 0.8)
-                    r = uniform(b, 1)
-                    g = 1 - r - b
-                setattr(obj, self.display_type, (r, g, b, 1))
+            if self.prop_name not in obj:
+                continue
+            groups[obj[self.prop_name]].append(obj)
+        hsv_base = Color((1, 0, 0))
+        for objects, hue in zip(groups.values(), np.linspace(0, 1, len(groups.keys()) + 1)):
+            hsv = hsv_base.copy()
+            hsv.h = hue
+            for obj in objects:
+                setattr(obj, self.display_type, (hsv.r, hsv.g, hsv.b, 1))
         return {"FINISHED"}
