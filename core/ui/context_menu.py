23b720aa3a0295f4cbfb9963d2cf4724a1cf5c49
Gorgious56 <nathan.hild@gmail.com>
2022-11-14 09:08:43 +0100

    You can now hide all collections of the active object from the right click context menu in the viewport

--- core/ui/context_menu.py
+++ core/ui/context_menu.py
@@ -1,11 +1,24 @@
 import bpy
 
 
+def draw_object_context_menu_appends(self, context):
+    layout = self.layout
+    active_obj = context.active_object
+    if active_obj:
+        layout.separator()
+
+        op = layout.operator("gu.collections_state_toggle", text="Hide All Collections", icon="HIDE_OFF")
+        op.state = True
+        op.attribute = "hide_viewport"
+
+
 def register():
     if hasattr(bpy.types, "UI_MT_button_context_menu"):
         pass
+    bpy.types.VIEW3D_MT_object_context_menu.append(draw_object_context_menu_appends)
 
 
 def unregister():
     if hasattr(bpy.types, "UI_MT_button_context_menu"):
         pass
+    bpy.types.VIEW3D_MT_object_context_menu.remove(draw_object_context_menu_appends)
