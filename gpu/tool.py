a744951e5d823abce7169cbcd9d07162360686a0
Gorgious56 <nathan.hild@gmail.com>
2023-01-31 09:42:03 +0100

    Face dots now appear too

--- gpu/tool.py
+++ gpu/tool.py
@@ -27,13 +27,14 @@ class MeshDrawer:
         cls.installed = None
 
     def __call__(self, context):
-        bgl.glLineWidth(2)
-        bgl.glPointSize(6)
         bgl.glEnable(bgl.GL_BLEND)
         bgl.glEnable(bgl.GL_LINE_SMOOTH)
 
         obj = context.active_object
         if obj is None or obj.type != "MESH" or not obj.GUProps.gpu.draw_mesh:
+            return
+
+        if obj.mode != "EDIT":
             return
 
         white = (1, 1, 1, 1)
@@ -43,8 +44,13 @@ class MeshDrawer:
         red_t = (1, 0.2, 0.322, 0.1)
         blue = (0.157, 0.565, 1, 1)
         blue_t = (0.157, 0.565, 1, 0.1)
-        grey = (0.2, 0.2, 0.2, 1)
+        grey = (0.4, 0.4, 0.4, 1)
         black = (0, 0, 0, 1)
+        orange = (1, 0.62, 0, 1)
+        pink = (0.8, 0.3, 0.8, 1)
+
+        color_unselected = pink
+        color_selected = red
 
         self.shader = gpu.shader.from_builtin("3D_UNIFORM_COLOR")
 
@@ -53,43 +59,63 @@ class MeshDrawer:
         unselected_edges = []
         selected_vertices = []
         unselected_vertices = []
+        selected_face_centers = []
+        unselected_face_centers = []
 
-        if obj.mode == "EDIT":
-            bm = bmesh.from_edit_mesh(obj.data)
+        depsgraph = context.evaluated_depsgraph_get()
+        obj = obj.evaluated_get(depsgraph)
 
-            for vertex in bm.verts:
-                co = tuple(obj.matrix_world @ vertex.co)
-                verts.append(co)
-                if vertex.hide:
+        bm = bmesh.from_edit_mesh(obj.data)
+        matrix_world = obj.matrix_world
+
+        for vertex in bm.verts:
+            co = tuple(matrix_world @ vertex.co)
+            verts.append(co)
+            if vertex.hide:
+                continue
+
+            if vertex.select:
+                selected_vertices.append(co)
+            else:
+                unselected_vertices.append(co)
+
+        for edge in bm.edges:
+            edge_indices = [v.index for v in edge.verts]
+            if edge.hide:
+                continue
+            if edge.select:
+                selected_edges.append(edge_indices)
+            else:
+                unselected_edges.append(edge_indices)
+
+        select_mode = bm.select_mode
+
+        def batch_shader(color, size, *args, **kwargs):
+            if args[0] == "LINES":
+                bgl.glLineWidth(size)
+            elif args[0] == "POINTS":
+                bgl.glPointSize(size)
+            batch = batch_for_shader(self.shader, *args, **kwargs)
+            self.shader.uniform_float("color", color)
+            batch.draw(self.shader)
+
+        batch_shader(color_unselected, 2, "LINES", {"pos": verts}, indices=unselected_edges)
+        batch_shader(color_selected, 3, "LINES", {"pos": verts}, indices=selected_edges)
+
+        batch_shader(black, 4, "POINTS", {"pos": unselected_vertices})
+        if "VERT" in select_mode:
+            batch_shader(color_selected, 8, "POINTS", {"pos": selected_vertices})
+
+        if "FACE" in select_mode:
+            for face in bm.faces:
+                if face.hide:
                     continue
-
-                if vertex.select:
-                    selected_vertices.append(co)
+                if face.select:
+                    selected_face_centers.append(matrix_world @ face.calc_center_median())
                 else:
-                    unselected_vertices.append(co)
+                    unselected_face_centers.append(matrix_world @ face.calc_center_median())
 
-            for edge in bm.edges:
-                edge_indices = [v.index for v in edge.verts]
-                if edge.hide:
-                    continue
-                if edge.select:
-                    selected_edges.append(edge_indices)
-                else:
-                    unselected_edges.append(edge_indices)
+            batch_shader(color_unselected, 5, "POINTS", {"pos": unselected_face_centers})
+            batch_shader(color_selected, 8, "POINTS", {"pos": selected_face_centers})
 
-            batch = batch_for_shader(self.shader, "LINES", {"pos": verts}, indices=unselected_edges)
-            self.shader.bind()
-            self.shader.uniform_float("color", black)
-            batch.draw(self.shader)
-
-            batch = batch_for_shader(self.shader, "LINES", {"pos": verts}, indices=selected_edges)
-            self.shader.uniform_float("color", red)
-            batch.draw(self.shader)
-
-            batch = batch_for_shader(self.shader, "POINTS", {"pos": unselected_vertices})
-            self.shader.uniform_float("color", black)
-            batch.draw(self.shader)
-
-            batch = batch_for_shader(self.shader, "POINTS", {"pos": selected_vertices})
-            self.shader.uniform_float("color", red)
-            batch.draw(self.shader)
+        self.shader.bind()
