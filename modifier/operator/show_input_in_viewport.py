4c74404bfa1f23e6dee40d5cd0a46bdbfe3fb8a1
Gorgious <nathan.hild@gmail.com>
2023-02-12 12:39:08 +0100

    You can now hide GN inputs from multiple objects simultaneously

--- modifier/operator/show_input_in_viewport.py
+++ modifier/operator/show_input_in_viewport.py
@@ -5,26 +5,27 @@ from bpy.props import StringProperty
 class GU_OT_modifier_show_input_in_viewport(bpy.types.Operator):
     bl_idname = "gu.modifier_show_input_in_viewport"
     bl_label = "Show Modifier Input in Viewort"
-    bl_options = {"REGISTER", "UNDO"}
+    bl_options = {"UNDO"}
 
     modifier_name: StringProperty()
     input_identifier: StringProperty(default="")
 
     def execute(self, context):
-        props = context.active_object.GUProps.modifier.modifier_inputs
-        props_mod = props.get(self.modifier_name)
-        if not props_mod:
-            props_mod = props.add()
-            props_mod.name = self.modifier_name
-        if self.input_identifier:
-            inp = props_mod.inputs.get(self.input_identifier)
-            if not inp:
-                inp = props_mod.inputs.add()
-                inp.name = self.input_identifier
-            inp.show = False
-        else:
-            for inp in props_mod.inputs:
-                inp.show = True
+        for obj in context.selected_objects:
+            props = obj.GUProps.modifier.modifier_inputs
+            props_mod = props.get(self.modifier_name)
+            if not props_mod:
+                props_mod = props.add()
+                props_mod.name = self.modifier_name
+            if self.input_identifier:
+                inp = props_mod.inputs.get(self.input_identifier)
+                if not inp:
+                    inp = props_mod.inputs.add()
+                    inp.name = self.input_identifier
+                inp.show = False
+            else:
+                for inp in props_mod.inputs:
+                    inp.show = True
 
         self.remove_unused(context, props)
 
