6e0ae2498a3f2b667013751c97a9de23a6ca49e3
Gorgious <nathan.hild@gmail.com>
2023-02-26 12:29:34 +0100

    Run black on script

--- attribute/copy_color_to_clipboard.py
+++ attribute/copy_color_to_clipboard.py
@@ -1,31 +1,30 @@
-import bpy
-import bmesh
-import platform
-import subprocess
+if __name__ == "__main__":
+    import bpy
+    import bmesh
+    import platform
+    import subprocess
 
-obj = bpy.context.active_object
-bm = bmesh.from_edit_mesh(obj.data)
+    obj = bpy.context.active_object
+    bm = bmesh.from_edit_mesh(obj.data)
 
+    def copy2clip(txt):
+        cmd = "echo " + txt.strip()
+        if platform.system() == "Linux":
+            cmd += "|xclip"
+        elif platform.system() == "Darwin":
+            cmd += "|pbcopy"
+        else:
+            cmd += "|clip"
+        return subprocess.check_call(cmd, shell=True)
 
-def copy2clip(txt):
-    cmd = "echo " + txt.strip()
-    if platform.system() == "Linux":
-        cmd += "|xclip"
-    elif platform.system() == "Darwin":
-        cmd += "|pbcopy"
-    else:
-        cmd += "|clip"
-    return subprocess.check_call(cmd, shell=True)
+    for v in bm.verts:
+        if v.select:
+            idx = v.index
+            bpy.ops.object.editmode_toggle()
 
-
-for v in bm.verts:
-    if v.select:
-        idx = v.index
-        bpy.ops.object.editmode_toggle()
-
-        attribute = obj.data.attributes["Color"]
-        attribute_value = attribute.data[idx].color
-        print([c for c in attribute_value])
-        copy2clip(str([c for c in attribute_value]))
-        bpy.ops.object.editmode_toggle()
-        break
+            attribute = obj.data.attributes["Color"]
+            attribute_value = attribute.data[idx].color
+            print([c for c in attribute_value])
+            copy2clip(str([c for c in attribute_value]))
+            bpy.ops.object.editmode_toggle()
+            break
