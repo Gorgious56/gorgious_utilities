ac90601c25b4420c6dca5676dc6bcf0f211971de
Gorgious56 <nathan.hild@gmail.com>
2023-06-07 11:02:20 +0200

    Only remove ifc objects when clearing ifc data

--- ifc/clean/clear_ifc_data.py
+++ ifc/clean/clear_ifc_data.py
@@ -12,24 +12,8 @@ class GU_OT_IFC_PCV_store_settings(bpy.types.Operator):
             bpy.ops.bim.purge_ifc_links()
         except AttributeError:
             pass
-        objs_to_delete = set(bpy.data.objects)
-        for obj in bpy.data.objects:
-            if obj in context.selected_objects or (
-                hasattr(obj, "point_cloud_visualizer") and obj.point_cloud_visualizer.load.filepath
-            ):
-                remove_from_all_collections(obj, link_in_scene_collection=True)
-                try:
-                    objs_to_delete.remove(obj)
-                except:
-                    pass
-                if clip_obj := obj.point_cloud_visualizer.shader.clip_planes_from_bbox_object:
-                    remove_from_all_collections(clip_obj, link_in_scene_collection=True)
-                    try:
-                        objs_to_delete.remove(clip_obj)
-                    except:
-                        pass
-
-        bpy.data.batch_remove(objs_to_delete)
-        bpy.data.batch_remove(bpy.data.collections)
+        collection = next((c for c in bpy.data.collections if c.name.startswith("IfcProject")), None)
+        if collection:
+            bpy.data.collections.remove(collection)
         bpy.ops.outliner.orphans_purge(do_local_ids=True, do_linked_ids=True, do_recursive=True)
         return {"FINISHED"}
