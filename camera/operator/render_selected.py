1e5a8ff1b55456ff1531709dcc5eccf179dd6770
Gorgious56 <nathan.hild@gmail.com>
2023-11-23 11:01:04 +0100

    You can now add a prefix to output selected camera renders

--- camera/operator/render_selected.py
+++ camera/operator/render_selected.py
@@ -1,11 +1,21 @@
+import typing
 import bpy
 from pathlib import Path
+
+from bpy.types import Context, Event
 
 
 class GU_OT_cameras_render_selected(bpy.types.Operator):
     bl_idname = "cameras.render_selected"
     bl_label = "Render Selected Cameras"
     bl_options = {"UNDO"}
+    prefix: bpy.props.StringProperty(name="Prefix", options={"SKIP_SAVE"})
+
+    def invoke(self, context: Context, event: Event):
+        if event.shift:
+            return context.window_manager.invoke_props_dialog(self)
+        else:
+            return self.execute(context)
 
     def execute(self, context):
         selected_cameras = [o for o in context.selected_objects if o.type == "CAMERA"]
@@ -13,8 +23,20 @@ class GU_OT_cameras_render_selected(bpy.types.Operator):
         layer = context.view_layer
         old_cam = scene.camera
         folder = Path(bpy.data.filepath).parent
+        if scene.node_tree is None:
+            print(
+                "Error : Please add a Compositing tree node to the compositor workspace"
+            )
+            return {"FINISHED"}
         compositor_nodes = scene.node_tree.nodes
-        layer_node = next((n for n in compositor_nodes if isinstance(n, bpy.types.CompositorNodeRLayers)), None)
+        layer_node = next(
+            (
+                n
+                for n in compositor_nodes
+                if isinstance(n, bpy.types.CompositorNodeRLayers)
+            ),
+            None,
+        )
         if layer_node is None:
             print("Error : Please add a View Layer node to the compositor workspace")
             return {"FINISHED"}
@@ -23,10 +45,16 @@ class GU_OT_cameras_render_selected(bpy.types.Operator):
         layer_node.layer = layer.name
         for cam in selected_cameras:
             scene.camera = cam
-            output_filepath = folder / "Renders" / (cam.name + ".png")
+            file_name = ""
+            if self.prefix:
+                file_name = str(self.prefix) + "_"
+            file_name += cam.name + ".png"
+            output_filepath = folder / "Renders" / file_name
             scene.render.filepath = str(output_filepath)
 
-            bpy.ops.render.render(write_still=True, use_viewport=True, scene=scene.name, layer=layer.name)
+            bpy.ops.render.render(
+                write_still=True, use_viewport=True, scene=scene.name, layer=layer.name
+            )
         print("Finished rendering")
         scene.camera = old_cam
         scene.render.filepath = old_filepath
