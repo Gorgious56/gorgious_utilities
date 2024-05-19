2063f141a60f8b339a01b467b40a67a9f6ca44c7
Gorgious <nathan.hild@gmail.com>
2024-05-19 09:19:34 +0200

    Make the modifier ops more compact

--- modifier/ui/menu.py
+++ modifier/ui/menu.py
@@ -2,23 +2,24 @@ import bpy
 from gorgious_utilities.core.ui.menu_factory import GU_Menu
 
 
+class GU_MT_modifier_ops(bpy.types.Menu):
+    bl_idname = "GU_MT_modifier_ops"
+    bl_label = "GU Ops"
+
+    def draw(self, context):
+        layout = self.layout
+        layout.operator("modifier.replace_gn_a_with_gn_b", icon="NODETREE")
+        layout.operator("modifier.replace_output_name", icon="GROUP_VCOL")
+        layout.operator("gu.select_same_gn", icon="RESTRICT_SELECT_OFF")
+
+        layout.operator("gu.select_used_objects", icon="LINKED", text="Select Used Objects")
+        layout.operator("gu.select_where_i_am_used", icon="LINKED", text="Select Objects Using Me")
+
+
 def draw_modifiers_properties(self, context):
     layout = self.layout
-    row = layout.row(align=True)
-    row.operator("modifier.replace_gn_a_with_gn_b", icon="NODETREE")
-    row.operator("modifier.replace_output_name", icon="GROUP_VCOL")
-    row.operator("gu.select_same_gn", icon="RESTRICT_SELECT_OFF")
 
-    row = layout.row(align=True)
-    sub_row = row.row(align=True)
-    sub_row.operator("gu.select_used_objects", icon="LINKED", text="Select Used Objects")
-    sub_row.operator("gu.select_where_i_am_used", icon="LINKED", text="Select Objects Using Me")
-    # op = sub_row.operator("object.make_links_data", text="Copy", icon="PASTEDOWN")
-    # op.type = "MODIFIERS"
-    # sub_row.operator("modifier.drive", icon="DECORATE_DRIVER")
-    # sub_row.operator("modifier.sync", text="Synch", icon="UV_SYNC_SELECT")
-    # sub_row.enabled = bool(len(context.view_layer.objects.selected) > 1 and context.active_object)
-    # row.operator("modifier.desync", text="Desynch", icon="UNLINKED")
+    layout.menu("GU_MT_modifier_ops")
 
 
 class GU_Menu_Modifier(GU_Menu):
