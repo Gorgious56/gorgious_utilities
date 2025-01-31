4d49d2e9f2a4194fbf649f755b27f0e2a4a82967
Gorgious56 <nathan.hild@gmail.com>
2025-01-31 17:39:05 +0100

    Remove gpu drawing code

--- core/preferences/ui.py
+++ core/preferences/ui.py
@@ -1,14 +1,8 @@
 from bpy.types import AddonPreferences
-from bpy.props import PointerProperty, BoolProperty, CollectionProperty
-
-from gorgious_utilities.gpu.prop import GPUPreferences
 
 
 class AssetBrowserUtilitiesAddonPreferences(AddonPreferences):
     bl_idname = "gorgious_utilities"
 
-    gpu: PointerProperty(type=GPUPreferences)
-
     def draw(self, context):
         layout = self.layout
-        self.gpu.draw(layout)
